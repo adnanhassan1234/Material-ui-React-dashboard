18f529cdd66d858b593779e1bd549be161b14e14
adnanhassan1234 <ah5404219@gmail.com>
2023-05-30 17:42:43 +0500

    csv

--- src/components/Charts/Barchart.js
+++ src/components/Charts/Barchart.js
@@ -11,47 +11,12 @@ import {
   Legend,
   ResponsiveContainer,
 } from "recharts";
-
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-  },
-];
+import { saveAs } from "file-saver";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 
 const BarCharts = () => {
-    const [chartData, setChartData] = useState([]);
+  const [chartData, setChartData] = useState([]);
   const [selectedOption, setSelectedOption] = useState("15"); // Default to 15 days
   const [isDropdownOpen, setIsDropdownOpen] = useState(false); // Dropdown open state
 
@@ -63,22 +28,67 @@ const BarCharts = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
 
+  // downloaded format csv , png and pdf
+  const handleDownload = (format) => {
+    if (format === "PNG") {
+      const chartContainer = document.getElementById("chart-container");
+      html2canvas(chartContainer).then((canvas) => {
+        canvas.toBlob((blob) => {
+          saveAs(blob, "chart.png");
+        });
+      });
+    } else if (format === "CSV") {
+      const csvData = chartData.map(
+        ({
+          country,
+          infected,
+          tested,
+          recovered,
+          deceased,
+          lastUpdatedDate,
+        }) => [country, infected, tested, recovered, deceased, lastUpdatedDate]
+      );
+      const csvRows = [
+        [
+          "Country",
+          "Infected",
+          "Tested",
+          "Recovered",
+          "Deceased",
+          "Last Updated Date",
+        ],
+        ...csvData,
+      ];
+      const csvContent = csvRows.map((row) => row.join(",")).join("\n");
+      const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8" });
+      saveAs(blob, "chart.csv");
+    } else if (format === "PDF") {
+      const chartContainer = document.getElementById("chart-container");
+      html2canvas(chartContainer).then((canvas) => {
+        const imgData = canvas.toDataURL("image/png");
+        const pdf = new jsPDF();
+        pdf.addImage(imgData, "PNG", 0, 0, 200, 100);
+        pdf.save("chart.pdf");
+      });
+    }
+  };
+
   const fetchChartData = async () => {
     try {
-      const response = await fetch(
-        "https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true"
-      );
+      const response = await fetch("http://localhost:3006/data");
       const data = await response.json();
 
       const extractedData = data.map((item) => ({
         country: item.country,
         infected: item.infected,
         recovered: item.recovered,
-        lastUpdatedApify: item.lastUpdatedApify,
+        tested: item.tested,
+        deceased: item.deceased,
+        lastUpdatedDate: item.lastUpdatedDate,
       }));
 
       extractedData.sort(
-        (a, b) => new Date(b.lastUpdatedApify) - new Date(a.lastUpdatedApify)
+        (a, b) => new Date(b.lastUpdatedDate) - new Date(a.lastUpdatedDate)
       );
 
       const filteredData = extractedData.slice(0, parseInt(selectedOption));
@@ -94,39 +104,50 @@ const BarCharts = () => {
 
   return (
     <>
-      <Box sx={{ p: 3, backgroundColor: "white", borderRadius:'4px' }}>
+      <Box sx={{p:2, backgroundColor: "white", borderRadius: "4px" }}>
         <Box sx={{ display: "flex", justifyContent: "space-between" }}>
           <Select
             value={selectedOption}
             onChange={handleOptionChange}
             sx={{ height: "35px" }}
           >
+            <MenuItem value="7">Last 7 Days</MenuItem>
             <MenuItem value="10">Last 10 Days</MenuItem>
             <MenuItem value="15">Last 15 Days</MenuItem>
             <MenuItem value="30">Last 30 Days</MenuItem>
           </Select>
           <CloudDownloadIcon
-            sx={{ fontSize: "33px", color:"#3A7AE9"}}
+            sx={{ fontSize: "33px", color: "#3A7AE9" }}
             onClick={handleCloudDownloadClick}
           />
         </Box>
         {isDropdownOpen && (
-          <Box sx={{ display: "flex", justifyContent: "flex-end", fontSize:'14px' }}>
-            <MenuItem value="item1">PNG</MenuItem>
-            <MenuItem value="item2">CSV</MenuItem>
-            <MenuItem value="item3">PDF</MenuItem>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-end",
+              fontSize: "14px",
+            }}
+          >
+            <MenuItem value="PNG" onClick={() => handleDownload("PNG")}>
+              PNG
+            </MenuItem>
+            <MenuItem value="CSV" onClick={() => handleDownload("CSV")}>
+              CSV
+            </MenuItem>
+            <MenuItem value="PDF" onClick={() => handleDownload("PDF")}>
+              PDF
+            </MenuItem>
           </Box>
-        )}{" "}
+        )}
         <br />
-        <ResponsiveContainer width="100%" aspect={2}>
+        <ResponsiveContainer id="chart-container" width="100%" aspect={2}>
           <BarChart data={chartData}>
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="country" />
             <YAxis />
             <Tooltip />
             <Legend />
-            {/* <Bar dataKey="pv" fill="#3A7AE9" />
-            <Bar dataKey="uv" fill="#82ca9d" /> */}
             <Bar dataKey="infected" fill="#3A7AE9" barSize={60} />
             <Bar dataKey="recovered" fill="#82ca9d" barSize={60} />
           </BarChart>
---
+++ src/components/Dashboard/DashboardCards.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Box, Grid, Typography } from '@mui/material';
+import styled from 'styled-components';
+import CoronavirusIcon from '@mui/icons-material/Coronavirus';
+
+
+const StyledCardContent = styled(Box)`
+  background-color: #3f84fc;
+  color: white;
+  border-radius: 4px;
+  padding: 25px 14px;
+`;
+
+const StyledCoronavirusIcon = styled(CoronavirusIcon)`
+  margin-bottom: 0;
+  && {
+    font-size: 50px;
+  }
+`;
+
+const DashboardCards = (props) => {
+  return (
+    <Grid item lg={3} md={6} sm={6} xs={12}>
+      <StyledCardContent>
+        <Box display="flex" alignItems="center">
+          <StyledCoronavirusIcon />
+          <Box ml={2}>
+            <Typography gutterBottom variant="h6" mb={0}>
+              {props.title}
+            </Typography>
+            <Typography variant="h6" mb={0}>
+              <b style={{ fontSize: '18px' }}>{props.totalInfected}</b>
+            </Typography>
+          </Box>
+        </Box>
+      </StyledCardContent>
+    </Grid>
+  );
+};
+
+export default DashboardCards;
