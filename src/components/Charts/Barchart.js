6524a26b0e866c0a79aa77dbef281a31f47cfb59
adnanhassan1234 <ah5404219@gmail.com>
2023-06-01 17:02:28 +0500

    table

--- src/components/Charts/Barchart.js
+++ src/components/Charts/Barchart.js
@@ -10,19 +10,24 @@ import {
   TableRow,
   TableCell,
   TablePagination,
-  TextField,
-  Button,
 } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import {
   BarChart,
   Bar,
+  LineChart,
+  Line,
+  AreaChart,
+  Area,
+  Pie,
   XAxis,
   YAxis,
   CartesianGrid,
   Tooltip,
   Legend,
   ResponsiveContainer,
+  PieChart,
+  Cell,
 } from "recharts";
 import CloudDownloadIcon from "@mui/icons-material/CloudDownload";
 import { useSnackbar } from "notistack";
@@ -34,9 +39,9 @@ const BarCharts = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const { enqueueSnackbar } = useSnackbar();
   const [tableData, setTableData] = useState([]);
-  // Pagination state variables
   const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(5); // Number of rows per page
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [visualizationType, setVisualizationType] = useState("bar");
 
   const handleFileUploads = async (event) => {
     const file = event.target.files[0];
@@ -100,7 +105,7 @@ const BarCharts = () => {
       return;
     }
 
-    const filteredData = updateData(selectedOptions); // Filter the data based on the selected number of days
+    const filteredData = updateData(selectedOptions);
 
     const dataToDownload = filteredData
       .map((item) => Object.values(item).join(","))
@@ -126,7 +131,6 @@ const BarCharts = () => {
     }
   }, [data, selectedOptions]);
 
-  // Pagination event handlers
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
@@ -141,22 +145,22 @@ const BarCharts = () => {
     <>
       <Box sx={{ p: 2, backgroundColor: "white", borderRadius: "4px" }}>
         <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          {/* Charts catagory types */}
           <Select
-            value={selectedOptions}
-            onChange={handleOptionChange}
-            sx={{ height: "35px" }}
+            value={visualizationType}
+            onChange={(event) => setVisualizationType(event.target.value)}
+            sx={{ height: "35px", width: "143px" }}
           >
-            <MenuItem value="7">Last 7 Days</MenuItem>
-            <MenuItem value="10">Last 10 Days</MenuItem>
-            <MenuItem value="15">Last 15 Days</MenuItem>
-            <MenuItem value="30">Last 30 Days</MenuItem>
+            <MenuItem value="bar">Bar Chart</MenuItem>
+            <MenuItem value="area">Area Chart</MenuItem>
+            <MenuItem value="line">Line Chart</MenuItem>
+            <MenuItem value="pie">Pie Chart</MenuItem>
           </Select>
           <CloudDownloadIcon
             sx={{ fontSize: "33px", color: "#3A7AE9" }}
             onClick={handleCloudDownloadClick}
           />
         </Box>
-
         {isDropdownOpen && (
           <Box
             sx={{
@@ -189,39 +193,52 @@ const BarCharts = () => {
           </Box>
         )}
         <br />
-        <input
-          type="file"
-          accept=".csv"
-          id="csv-file-inputs"
-          onChange={handleFileUploads}
-          style={{ display: "none" }}
-        />
-        <label htmlFor="csv-file-inputs">
-          <Box
-            component="span"
-            sx={{
-              display: "inline-block",
-              padding: "6px 12px",
-              backgroundColor: "#f5f5f5",
-              borderRadius: "4px",
-              cursor: "pointer",
-              userSelect: "none",
-              marginBottom: "16px",
-            }}
-          >
-            Upload CSV File
-          </Box>
-        </label>
+        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          <input
+            type="file"
+            accept=".csv"
+            id="csv-file-inputs"
+            onChange={handleFileUploads}
+            style={{ display: "none" }}
+          />
+          <label htmlFor="csv-file-inputs">
+            <Box
+              component="span"
+              sx={{
+                display: "inline-block",
+                padding: "6px 12px",
+                backgroundColor: "#f5f5f5",
+                borderRadius: "4px",
+                cursor: "pointer",
+                userSelect: "none",
+                marginBottom: "16px",
+              }}
+            >
+              Upload CSV File
+            </Box>
+          </label>
 
+          {isInvalidFile && (
+            <Typography variant="body1" sx={{ color: "red" }}>
+              Please upload a valid CSV file
+            </Typography>
+          )}
+          {tableData.length > 0 && (
+            <Select
+              value={selectedOptions}
+              onChange={handleOptionChange}
+              sx={{ height: "35px", width: "143px" }}
+            >
+              <MenuItem value="7">Last 7 Days</MenuItem>
+              <MenuItem value="10">Last 10 Days</MenuItem>
+              <MenuItem value="15">Last 15 Days</MenuItem>
+              <MenuItem value="30">Last 30 Days</MenuItem>
+            </Select>
+          )}
+        </Box>{" "}
         <br />
-        {isInvalidFile && (
-          <Typography variant="body1" sx={{ color: "red" }}>
-            Please upload a valid CSV file
-          </Typography>
-        )}
-
-        {/* barchart */}
-        {data.length > 0 && (
+        {/* Bar charts */}
+        {visualizationType === "bar" && data.length > 0 && (
           <ResponsiveContainer width="100%" aspect={2}>
             <BarChart data={updateData(selectedOptions)}>
               <CartesianGrid strokeDasharray="3 3" />
@@ -234,50 +251,88 @@ const BarCharts = () => {
             </BarChart>
           </ResponsiveContainer>
         )}
-        {/* dataTable show */}
-        {tableData.length > 0 && (
-          <Typography color="text.primary" variant="h6">
-            Analytics Report
-          </Typography>
-        )}
-        {tableData.length > 0 && (
-          <Box sx={{ overflowY: "scroll" }}>
-            <TableContainer>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>#</TableCell>
-                    {Object.keys(tableData[0]).map((key) => (
-                      <TableCell key={key}>{key}</TableCell>
-                    ))}
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {tableData
-                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                    .map((item, index) => (
-                      <TableRow key={index}>
-                        <TableCell>{index + 1}</TableCell>
-                        {Object.values(item).map((value, idx) => (
-                          <TableCell key={idx}>{value}</TableCell>
-                        ))}
-                      </TableRow>
-                    ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-            {tableData.length > 0 && (
-              <TablePagination
-                rowsPerPageOptions={[5, 10, 25]}
-                component="div"
-                count={tableData.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
+        {/* Line Charts */}
+        {visualizationType === "line" && data.length > 0 && (
+          <ResponsiveContainer width="100%" aspect={2}>
+            <LineChart data={updateData(selectedOptions)}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey={Object.keys(data[0])[0]} />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line
+                type="monotone"
+                dataKey={Object.keys(data[0])[1]}
+                stroke="#3F84FC"
               />
-            )}
-          </Box>
+              <Line
+                type="monotone"
+                dataKey={Object.keys(data[0])[2]}
+                stroke="#82ca9d"
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        )}
+        {/* Area chart */}
+        {visualizationType === "area" && data.length > 0 && (
+          <ResponsiveContainer width="100%" aspect={2}>
+            <AreaChart data={updateData(selectedOptions)}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey={Object.keys(data[0])[0]} />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Area
+                type="monotone"
+                dataKey={Object.keys(data[0])[1]}
+                fill="#3F84FC"
+                stroke="#3F84FC"
+              />
+              <Area
+                type="monotone"
+                dataKey={Object.keys(data[0])[2]}
+                fill="#82ca9d"
+                stroke="#82ca9d"
+              />
+            </AreaChart>
+          </ResponsiveContainer>
+        )}
+        {/* table data */}
+        {tableData.length > 0 && (
+          <TableContainer>
+            <Typography color="text.primary" variant="h6">
+              Analytics Report
+            </Typography>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  {Object.keys(tableData[0]).map((header, index) => (
+                    <TableCell key={index}>{header}</TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {tableData
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((row, index) => (
+                    <TableRow key={index}>
+                      {Object.values(row).map((value, index) => (
+                        <TableCell key={index}>{value}</TableCell>
+                      ))}
+                    </TableRow>
+                  ))}
+              </TableBody>
+            </Table>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25]}
+              component="div"
+              count={tableData.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </TableContainer>
         )}
       </Box>
     </>
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -129,14 +129,14 @@ const Dashboard = () => {
           </Grid> */}
           <Grid container spacing={2}>
             {/* here date filed and download button in right side */}
-            <Grid item lg={6} md={6} sm={12} xs={12}>
+            <Grid item lg={6} md={12} sm={12} xs={12}>
               <BarCharts />
             </Grid>
-            <Grid item lg={6} md={6} sm={12} xs={12}>
+            <Grid item lg={6} md={12} sm={12} xs={12}>
               <LineCharts />
             </Grid>
             <Grid item lg={12}>
-              <ChartsTableData />
+              {/* <ChartsTableData /> */}
             </Grid>
           </Grid>
         </Container>
